2c25973c90d5effa5f437d61bb1798513b08118d
rf-castle <kouta.tashiro.2000@gmail.com>
2022-05-19 00:18:49 +0900

    rejectionが発生したときにプロセスが止まらないように修正

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,10 @@ import {Client} from 'discord.js';
 import {registerHandler} from './event';
 
 
+process.on('unhandledRejection', (error) => {
+    console.error(error);
+})
+
 async function main(){
     const client = new Client({intents: 32767});
     registerHandler(client);
